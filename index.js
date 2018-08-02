2e5b2da00a62b8a4e633ef8a827afa41d3dcb5de
Tomas Toledo <trtoledo1977@gmail.com>
2018-08-02 20:01:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // Write your solution here!
 
 const drivers = ["Milo", "Otis", "Garfield"]
+
 function destructivelyAppendDriver(name){
   drivers.push(name);
+}
+
+function destructivelyPrependDriver(name){
+  drivers.unshift(name)
 }
